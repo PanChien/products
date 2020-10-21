0672ff96a3dc84da9665858adeae920bd6f7371f
PanChien <zoo0605@hotmail.com>
2020-10-22 01:54:53 +0800

    add read file

--- products.py
+++ products.py
@@ -1,6 +1,21 @@
 #記帳程式(二維清單)
+#讀取檔案
 
 products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue #繼續
+		name, price = line.strip().split(',')
+		'''
+		s = line.strip().split(',')
+		name = s[0]
+		price = s[1]
+		'''
+		products.append([name, price])
+print(products)
+
+#讓使用者輸入資料
 while True:
 	name = input('請輸入商品名稱： ')
 	if name == 'q':		#跳出while迴圈
@@ -15,13 +30,14 @@ while True:
 	products.append([name, price])
 print(products)
 
+#印出所有購買記錄
 for p in products:
 	print(p[0], '的價格是', p[1])
 
+#寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + str(p[1]) + '\n')  
 		#字串不能跟整數作加減
 		#str(p[1]) 將整數轉字串
-
